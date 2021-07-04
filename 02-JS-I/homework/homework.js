701d8b9b7e6f274902e623067990a6c0270c0140
psanguina <psanguina@gmail.com>
2021-07-04 19:17:57 -0300

    finalizado 1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -203,19 +203,21 @@ return 'Hola ' + nombre + '!';
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  return alto * ancho
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
+  return lado * 4
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+return (base * altura)/2
 }
 
 
@@ -223,6 +225,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
+  return euro * 1.2
   
 }
 
@@ -232,9 +235,14 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   //Escribe tu código aquí
+  if(letra.length > 1){
+    return "Dato incorrecto"
+  }
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal"}
+   else{ return 'Dato incorrecto'
+  }
 }
-
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
